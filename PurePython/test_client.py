16b45d5e46806c57d799538fbdbe34fc157c4a4c
Casey Kennington <bakuzen@gmail.com>
2020-11-10 21:22:02 -0700

    audio streaming seems to be working

--- PurePython/test_client.py
+++ PurePython/test_client.py
@@ -1,4 +1,6 @@
 import socketio
+import time
+import threading
 
 sio = socketio.Client()
 
@@ -6,17 +8,14 @@ from MistyAPI import Robot
 import av
 import numpy as np
 
-robot_ip = '192.168.0.9'
+robot_ip = '192.168.1.137'
 robot = Robot(robot_ip)
 # robot.enable_avstream()
 # robot.stream_av()
 
 next_container = None
 
-
-
-@sio.event
-def connect():
+def consumer_thread():
     global next_container
     print("connected, starting stream")
     stream_path = 'rtsp://{}:1936'.format(robot_ip)
@@ -27,6 +26,12 @@ def connect():
         sio.emit('audio', frame.to_ndarray().astype(np.float32).tostring())
 
 @sio.event
+def connect():
+    t = threading.Thread(target=consumer_thread)
+    t.start()  
+
+
+@sio.event
 def my_message(data):
     print('message received with ', data)
 
@@ -35,5 +40,5 @@ def disconnect():
     print('disconnected from server')
 
 
-sio.connect('http://192.168.0.8:5000')
+sio.connect('http://192.168.1.135:5000')
 sio.wait()
--- PurePython/test_server.py
+++ PurePython/test_server.py
@@ -1,18 +1,61 @@
 import eventlet
 import socketio
 import pyaudio
+import threading
+import time
+import numpy as np
+from BlockingQueue import BlockingQueue
+queue = BlockingQueue(1000)
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio, static_files={
     '/': {'content_type': 'text/html', 'filename': 'index.html'}
 })
 
-p = pyaudio.PyAudio()
+# def add_silence():
+#     global queue
+#     noise = np.random.normal(0,0,100)
+#     while True:
+#         if queue.isEmpty():
+#             queue.enqueue(noise)
 
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=44100,
-                output=True)
+def consumer_thread():
+    global queue
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=44100,
+                    output=True)
+
+    while True:
+        item = queue.dequeue()
+        try: 
+            stream.write(item, exception_on_underflow=True)
+        except:
+            print("EXCEPTION!")
+            time.sleep(0.1)
+            p = pyaudio.PyAudio()
+            stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=44100,
+                    output=True)
+            
+        # if queue.isEmpty() and item is None:
+        #     stream.write(noise)
+        #     print("playing whitenoise")
+
+        # else:
+        #     try:
+                
+        #         print("playing audio")
+        #     except:
+        #         print("error")
+                
+        #         # stream.write(item)
+        #         print("playing audio")
+                
 
 @sio.event
 def connect(sid, environ):
@@ -20,13 +63,19 @@ def connect(sid, environ):
 
 @sio.on('audio')
 def my_message(sid, data):
-    global stream
-    print('receiving')
-    stream.write(data)
+    global queue
+    # print('receiving')
+    queue.enqueue(data)
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
 if __name__ == '__main__':
+    t = threading.Thread(target=consumer_thread)
+    t.start()  
+    # t = threading.Thread(target=add_silence)
+    # t.start()      
     eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
+    
+ 
